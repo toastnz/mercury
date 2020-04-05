099749890e57af96a75a2882e8e3f7fa4397c657
JD <jd@pinc.nz>
2020-04-05 21:11:07 +1200

    removed messages

--- app/_config.php
+++ app/_config.php
@@ -4,27 +4,12 @@ use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
 use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
 use SilverStripe\SiteConfig\SiteConfig;
-use SilverStripe\View\Requirements;
+use SilverStripe\Security\Security;
 // remove PasswordValidator for SilverStripe 5.0
 $validator = PasswordValidator::create();
 // Settings are registered via Injector configuration - see passwords.yml in framework
 Member::set_password_validator($validator);
 
-
-$config = SiteConfig::current_site_config();
-$data = $config->typeCSS;
-
-$primaryColour = substr($data, strpos($data, '--primary-colour:') + 17, 7);
-$secondaryColour = substr($data, strpos($data, '--secondary-colour:') + 19, 7);
-
-
-// Requirements::customCSS(
-//     <<<CSS
-//   .colour--primary{
-//       color: $primaryColour;
-//   }  
-// CSS
-// );
 
 $formats = [
     [
@@ -66,19 +51,28 @@ $formats = [
     ],
 ];
 
-
 TinyMCEConfig::get('cms')->enablePlugins(['hr', 'anchor']);
 
-TinyMCEConfig::get('cms')
-    ->addButtonsToLine(2, 'styleselect')
-    ->setOptions([
-        'importcss_append'        => true,
-        'style_formats'           => $formats,
-        'content_style'   =>
-        '.colour--primary {color: ' . $primaryColour . '; } '
-            . '.colour--secondary {color: ' . $secondaryColour . '; } '
-            . '.colour--white {color: #bbbbbb; } '
-            . '.button {padding:5px 10px !important;color:white;background: ' . $primaryColour . '; } '
-            . '.button--secondary {padding:5px 10px !important;color:white;background: ' . $secondaryColour . '; } '
-            . '.button--outline {padding:5px 10px !important;background:transparent;color:' . $primaryColour . ';border: 1px solid ' . $primaryColour . '; } '
-    ]);
+if (Security::database_is_ready()) {
+
+    $config = SiteConfig::current_site_config();
+
+    $data = $config->typeCSS;
+
+    $primaryColour = substr($data, strpos($data, '--primary-colour:') + 17, 7);
+    $secondaryColour = substr($data, strpos($data, '--secondary-colour:') + 19, 7);
+
+    TinyMCEConfig::get('cms')
+        ->addButtonsToLine(2, 'styleselect')
+        ->setOptions([
+            'importcss_append'        => true,
+            'style_formats'           => $formats,
+            'content_style'   =>
+            '.colour--primary {color: ' . $primaryColour . '; } '
+                . '.colour--secondary {color: ' . $secondaryColour . '; } '
+                . '.colour--white {color: #bbbbbb; } '
+                . '.button {padding:5px 10px !important;color:white;background: ' . $primaryColour . '; } '
+                . '.button--secondary {padding:5px 10px !important;color:white;background: ' . $secondaryColour . '; } '
+                . '.button--outline {padding:5px 10px !important;background:transparent;color:' . $primaryColour . ';border: 1px solid ' . $primaryColour . '; } '
+        ]);
+}
--- themes/mercury/templates/Layout/Page.ss
+++ themes/mercury/templates/Layout/Page.ss
@@ -3,8 +3,7 @@
 <section class="textBlock block">
 	<div class="textBlock__wrap row">
 		<div class="column">
-			$Content
-            $Form
+			$Form
 		</div>
 	</div>
 </section>
